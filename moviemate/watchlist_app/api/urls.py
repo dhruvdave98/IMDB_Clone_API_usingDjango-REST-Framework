ac0130b7ebed89d689b2bd0c87af5bfbdd54d972
Dhruv Dave <dhruvdave61@gmail.com>
2022-07-04 10:19:40 -0400

    Initial commit

---
+++ moviemate/watchlist_app/api/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import (WatchListAV, WatchDetailsAV, StreamPlatformAV,
+                                     StreamPlatformDetailsAV, ReviewList, ReviewDetail, ReviewCreate, StreamPlatformVS, UserReview, WatchListGV)
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
+
+urlpatterns = [
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', WatchDetailsAV.as_view(), name='movie-details'),
+    path('list2/', WatchListGV.as_view(), name='watch-list'),
+
+    path('', include(router.urls)),
+
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('<int:pk>/reviews/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
+
+    path('reviews/<str:username>/', UserReview.as_view(), name="user-review-detail"),
+
+
+]
